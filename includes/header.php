6f8bb3606d9495a956a1591c6d94ee250cf6484f
misaelbroertjes <misaelbroertjes@hotmail.com>
2017-12-08 23:29:56 +0100

    whitespaces fix header

--- includes/header.php
+++ includes/header.php
@@ -1,19 +1,1 @@
-<div id="header">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				<ul class="nav nav-tabs">
-  				<li class="nav-item">
-						<a class="nav-link <?php if($page == "home"){ echo "active"; }?>" href="<?php echo $root; ?>/" class="nav-link">Home</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link disabled <?php if($page == "adres"){ echo "active"; }?>" href="" class="nav-link">Adres</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link disabled <?php if($page == "nabezoek"){ echo "active"; }?>" href="" class="nav-link">Nabezoek</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
-</div>
+<div id="header">	<div class="container">		<div class="row">			<div class="col-sm-12">				<ul class="nav nav-tabs">                    <li class="nav-item">                        <a class="nav-link <?php if($page == "home"){ echo "active"; }?>" href="<?php echo $root; ?>/" class="nav-link">Home</a>                    </li>                    <li class="nav-item">                        <a class="nav-link disabled <?php if($page == "adres"){ echo "active"; }?>" href="" class="nav-link">Adres</a>                    </li>                    <li class="nav-item">                        <a class="nav-link disabled <?php if($page == "nabezoek"){ echo "active"; }?>" href="" class="nav-link">Nabezoek</a>                    </li>    			</ul>			</div>		</div>	</div></div>
